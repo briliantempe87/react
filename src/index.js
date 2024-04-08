63fb4f5715eff61aa0c11760953facd0e084584b
briliant firmansyah <briliantempe87.com>
2024-04-08 09:14:54 +0700

    img

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div>
+      <h1>p balap</h1>
+      <Food />
+      <Food />
+      <Food />
+      <Food />
+    </div>
+  );
+}
+
+function Food() {
+  return (
+    <div>
+      <img src="food/soto.jpeg" alt="" />
+      <h2>soto betawi</h2>
+      <p>abdul</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
